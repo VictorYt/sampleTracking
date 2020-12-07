83969ba31a0b7a04f3910d8c7e7b1978b93aecf0
Victor Ythier <victor.ythier@gmail.com>
2020-12-07 12:13:49 +0100

    Sample Tracking test

---
+++ sampleTracking.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+# -*- coding: utf8 -*-
+
+"""
+:Author: Victor Ythier
+:Date: 02.12.2020
+
+This script check all calling in file directory and report a resum table at specific position
+List of seven rs :
+    - rs1801133
+    - rs1045642
+    - rs1128503
+    - rs2306283
+    - rs9934438
+    - rs2108632
+    - rs1135840
+"""
+
+# IMPORT
+import os, glob
+import pandas as pd
+from collections import OrderedDict
+from scripts import sampleTracking_function
+from _version import __version__
+
+
+REFERENCE = OrderedDict([('rs1801133','G/G'),
+                         ('rs1045642','A/A'),
+                         ('rs1128503','A/A'),
+                         ('rs2306283','A/A'),
+                         ('rs9934438','G/G'),
+                         ('rs2108622','C/C'),
+                         ('rs4680','G/G'),
+                         ('rs1135840','C/C')])
+cols = list(REFERENCE.keys())
+cols.insert(0, 'Samples')
+dfFinal = pd.DataFrame(columns=cols)
+
+def main(dfinal):
+    """
+    Create a genotype summary table of all Variant to Table in the current directory
+    :return:
+    """
+    # List of file
+    all_samples = glob.glob("*.tsv")
+    runname = os.path.basename(os.getcwd())
+    output = runname + '.tsv'
+    for s in all_samples:
+        iterarray = None
+        df = pd.read_csv(s, header=0, sep="\t")
+        sample = s.replace(".tsv", "")
+        iterarray.append(sample)
+        for key, value in REFERENCE.items():
+            if key not in df['ID'].tolist():
+                iterarray.append(value)
+            else:
+                idx = df[df['ID']==key].index[0]
+                genotype = sampleTracking_function.getGenotype(df, idx)
+                iterarray.append(genotype)
+        iterdict = dict(zip(dfinal.columns, iterarray))
+        dfinal = dfinal.append(iterdict, ignore_index=True)
+
+    #dfinal = dfinal.groupby(dfinal.columns.tolist()).size().reset_index().rename(columns={0: 'records'})
+    dfinal = dfinal.set_index(dfinal.columns[0])
+    dup_info = dfinal.duplicated()
+    dup_info.to_csv("Duplication_information.tsv", sep="\t", header=['Duplication_information'])
+    dfinal.to_csv(output, sep="\t", index=True)
+
+# Execution
+if __name__ == "__main__":
+    main(dfFinal)
---
+++ scripts/sampleTracking_function.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf8 -*-
+
+"""
+:Author: Victor Ythier
+:Date: 24.09.2020
+
+This script content function for the RunWatcher script:
+    - getGenotype
+
+"""
+
+# IMPORT
+import pandas as pd
+
+
+def getGenotype(df, index):
+    """
+    Using the information about the rsID return the genotype in the wanted format
+    :param df: [DataFrame] The table containing Variant calling
+    :param index: [int] index of the rs ID
+    :return: [str] Genotype in the format NT/NT
+    """
+    geno = None
+    if df.iat[index, 6] == "[G*, A]":
+        geno = "G/A"
+    elif df.iat[index, 6] == "[A, A]":
+        geno = "A/A"
+    elif df.iat[index, 6] == "[A*, G]":
+        geno = "A/G"
+    elif df.iat[index, 6] == "[G, G]":
+        geno = "G/G"
+    elif df.iat[index, 6] == "[C*, T]":
+        geno = "C/T"
+    elif df.iat[index, 6] == "[T, T]":
+        geno = "T/T"
+    elif df.iat[index, 6] == "[G*, C]":
+        geno = "G/C"
+    elif df.iat[index, 6] == "[C, C]":
+        geno = "C/C"
+    return geno
+
